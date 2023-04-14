25171c6227b2d97715f9f0ba2d6e95038a21a65f
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-04-14 14:19:53 +0300

    Update pass_fail.py

--- pass_fail.py
+++ pass_fail.py
@@ -1,16 +1,31 @@
-import os
+###################################################################################################
+#
+# Copyright (C) 2020 Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+"""
+Check the test results
+"""
 import configparser
+import os
 
-#config_path = r'C:\Users\aturhal\Desktop\ai\source\test_config.conf'
+# config_path = r'C:\Users\aturhal\Desktop\ai\source\test_config.conf'
 config_path = r'/home/asyaturhal/desktop/ai/test_config.conf'
 config = configparser.ConfigParser()
 config.read(config_path)
 log_path = r'/home/asyaturhal/desktop/ai/log_diff'
-#log_path = r'C:\Users\aturhal\Desktop\test_logs'
+# log_path = r'C:\Users\aturhal\Desktop\test_logs'
 log_path = log_path + '/' + sorted(os.listdir(log_path))[-1]
 
+
 def check_top_value(file, threshold):
-    with open(file, 'r') as f:
+    """
+    Compare Top1 value with threshold
+    """
+    with open(file, 'r', encoding='utf-8') as f:
 
         model_name = file.split('/')[-1].split('.')[0]
         # Read all lines in the file
@@ -19,22 +34,21 @@ def check_top_value(file, threshold):
         top1 = lines[-1].split()
         epoch_num = int(top1[0])
         top1_diff = float(top1[1])
-        #top5_diff = float(top1[2])
+        # top5_diff = float(top1[2])
 
     if top1_diff < threshold:
-         
-        print("\033[31m\u2718\033[0m Test failed for {} since in Top1 value changed {} at {}th epoch.".format(model_name, top1_diff, epoch_num))
+        print(f"\033[31m\u2718\033[0m Test failed for {model_name} since in"
+              f" Top1 value changed {top1_diff} at {epoch_num}th epoch.")
         return False
-    
-    else:
+    print(f"\033[31m\u2718\033[0m Test failed for {model_name} since in"
+          f" Top1 value changed {top1_diff} at {epoch_num}th epoch.")
+    return True
 
-        print("\033[32m\u2714\033[0m Test passed for {} since in Top1 value changed {} at {}th epoch.".format(model_name, top1_diff, epoch_num))
-        return True
-    
-for logs in os.listdir(log_path):
+
+for logs in sorted(os.listdir(log_path)):
     if logs in config:
-        threshold = float(list(config['{}'.format(logs)]["threshold"])[0])
+        threshold_temp = float(list(config['{logs}']["threshold"])[0])
     else:
-        threshold = 0
+        threshold_temp = 0
     logs = log_path + '/' + str(logs)
-    check_top_value(logs, threshold)
+    check_top_value(logs, threshold_temp)
