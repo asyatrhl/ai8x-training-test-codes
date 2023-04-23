81f8d3067f906d653dcdb1b842ff75ef80a02387
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-04-23 20:24:17 +0300

    Update create_test_script.py

--- create_test_script.py
+++ create_test_script.py
@@ -9,6 +9,7 @@
 """
 Create training bash scripts for test
 """
+import configparser
 import os
 
 
@@ -19,6 +20,10 @@ def joining(lst):
     join_str = ' '.join(lst)
     return join_str
 
+
+config_path = r'/home/asyaturhal/desktop/ai/test_config.conf'
+config = configparser.ConfigParser()
+config.read(config_path)
 
 # Folder containing the files to be concatenated
 folder_path = (
@@ -38,34 +43,42 @@ output_file_path = (
 log_file_names = []
 
 # Loop through all files in the folder
-with open(output_file_path, "w", encoding='utf-8') as output_file:
-    for filename in os.listdir(folder_path):
-        # Check if the file is a train file
+with open(output_file_path, "w") as output_file:
+    for filename in os.listdir(script_path):
+        # Check if the file is a text file
         if filename.startswith("train"):
-            with open(os.path.join(folder_path, filename), encoding='utf-8') as input_file:
+            # Open the file and read its contents
+            with open(os.path.join(script_path, filename)) as input_file:
                 contents = input_file.read()
 
-            temp = contents.split()
-            temp.insert(1, "\n")
-            i = temp.index('--epochs')
-            j = temp.index('--model')
-            k = temp.index('--dataset')
+                temp = contents.split()
+                temp.insert(1, "\n")
+                i = temp.index('--epochs')
+                j = temp.index('--model')
+                k = temp.index('--dataset')
 
-            log_name = temp[j+1] + '-' + temp[k+1]
+                log_name = temp[j+1] + '-' + temp[k+1]
+                log_file_names.append(filename[:-3])
 
-            log_file_names.append(filename[:-3])
+                # temp[i+1] = str(int(temp[i+1])*10/100) 
+                temp[i+1] = config[f'{log_name}']["epoch"]
 
-            if '--deterministic' not in temp:
-                temp.insert(-2, '--deterministic')
+                if '--deterministic' not in temp:
+                    temp.insert(-2, '--deterministic')
 
-            temp.insert(-1, '--name ' + log_name)
+                temp.insert(-1, '--name ' + log_name)
+                
+                data_name = temp[k+1]
+                if data_name in config:
+                    path_data = config[f'{data_name}']["data_path"]
+                    temp.insert(-1, '--data ' + path_data)
 
-            # temp[i+1] = str(int(temp[i+1])*10/100)
-            temp[i+1] = str(5)
-            temp.append("\n")
-            contents = joining(temp)
+                temp.append("\n")
+                contents = joining(temp)
+                # Replace the number in the "--epochs" script
 
-            output_file.write(contents)
+                # Write the contents to the output file
+                output_file.write(contents)
 
 if "train_test" in log_file_names:
     log_file_names.remove("train_test")
